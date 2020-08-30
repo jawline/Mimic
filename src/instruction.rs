16cd362931a9bbed59d6ad7737bee33c5b6be8dc
Blake Loring <blake@parsed.uk>
2020-08-30 23:25:58 +0800

    Implement a third opcode

--- src/instruction.rs
+++ src/instruction.rs
@@ -39,6 +39,12 @@ impl InstructionData {
     a.wide_reg_dst = r;
     a
   }
+
+  pub fn wide_dst_small_in(r: WideRegister, l: SmallWidthRegister) -> InstructionData {
+    let mut a = InstructionData::wide_dst(r);
+    a.small_reg_one = l;
+    a
+  }
 }
 
 /// The instruction struct contains the implementation of and metadata on an instruction
@@ -56,11 +62,27 @@ pub fn no_op(registers: &mut Registers, memory: &mut Box<dyn MemoryChunk>, addit
 }
 
 /// Load immediate loads a 16 bit value from memory and places it in a register
-pub fn ld_imm_reg(registers: &mut Registers, memory: &mut Box<dyn MemoryChunk>, additional: &InstructionData) {
+pub fn ld_imm_reg16(registers: &mut Registers, memory: &mut Box<dyn MemoryChunk>, additional: &InstructionData) {
+  //Load the 16 bit value after the opcode and store it to the dst register
   let imm_val = memory.read_u16(registers.pc() + 1);
   registers.write_r16(additional.wide_reg_dst, imm_val);
-  registers.write_r16(WideRegister::PC, registers.pc() + 1);
+
+  //Increment the PC by three once finished
+  registers.write_r16(WideRegister::PC, registers.pc() + 3);
 }
+
+pub fn ld_reg8_mem_reg16(registers: &mut Registers,
+  memory: &mut Box<dyn MemoryChunk>,
+  additional: &InstructionData) {
+
+  //Store the provided 8-bit register to the location pointed to by the 16-bit dst register
+  let reg_val = registers.read_r8(additional.small_reg_one);
+  let mem_dst = registers.read_r16(additional.wide_reg_dst);
+  memory.write_u16(mem_dst, reg_val);
+
+  //Increment the PC by three once finished
+  registers.write_r16(WideRegister::PC, registers.pc() + 1);
+} 
 
 pub fn instruction_set() -> Vec<Instruction> {
 
@@ -72,10 +94,17 @@ pub fn instruction_set() -> Vec<Instruction> {
   };
 
   let load_imm_bc = Instruction {
-    execute: crate::instruction::ld_imm_reg,
+    execute: crate::instruction::ld_imm_reg16,
     timings: (3, 12),
     text: "ld_imm_bc".to_string(),
     data: InstructionData::wide_dst(WideRegister::BC)
+  };
+
+  let load_bc_a = Instruction {
+    execute: crate::instruction::ld_reg8_mem_reg16,
+    timings: (1, 8),
+    text: "ld_(bc)_a".to_string(),
+    data: InstructionData::wide_dst_small_in(WideRegister::BC, SmallWidthRegister::A)
   };
 
   vec![no_op, load_imm_bc]
