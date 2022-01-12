683c131e329343faf2cba7e5a176dfab05611623
Blake Loring <blake@parsed.uk>
2022-01-12 22:07:23 +0800

    Sound new

--- src/sound.rs
+++ src/sound.rs
@@ -6,26 +6,28 @@ use serde::{Deserialize, Serialize};
 use std::sync::mpsc;
 use std::sync::mpsc::{Receiver, Sender};
 use std::marker::PhantomData;
+use cpal::Sample;
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use std::result::Result;
+use std::error::Error;
 
-pub struct Sound<T>{
+pub struct Sound<T: Sample>{
     _t: PhantomData<T>,
 }
 
-impl Sound<T> {
+impl<T: Sample> Sound<T> {
 
-    pub fn new() -> Self<T> {
+    pub fn new<R: Sample>() -> Result<Sound<R>, Box<dyn Error>> {
       let host = cpal::default_host();
       let device = host.default_output_device().ok_or("no device found")?;
       let config = device.default_output_config().unwrap();
-      match config.sample_format() {
-          cpal::SampleFormat::F32 => Sound { _t: Phanton::new() },
-          cpal::SampleFormat::I16 => Sound { _t: Phantom::new() },
-          cpal::SampleFormat::U16 => Sound { _t: Phantom::new() }
-      }
+      Ok(match config.sample_format() {
+          cpal::SampleFormat::F32 => Sound { _t: PhantomData },
+          cpal::SampleFormat::I16 => Sound { _t: PhantomData },
+          cpal::SampleFormat::U16 => Sound { _t: PhantomData }
+      })
     }
-}
 
-impl <T> Sound<T> {
     pub fn step(&mut self, cpu: &mut Cpu, mem: &mut GameboyState) {
 
         // We send samples from the emulator to the sound thread using channels
